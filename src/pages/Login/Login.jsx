a06f7ebc3b1e00eae0dbe43a98a1384245a11cba
mhscan <mr.rezaeeian@gmail.com>
2023-02-16 19:21:58 +0330

    login page create

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,103 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "../../Components/Form/Button";
+import Input from "../../Components/Form/Input";
+import { useForm } from "../../hooks/useForm";
+
+import {
+  requiredValidator,
+  minValidator,
+  maxValidator,
+  emailValidator,
+} from "../../validators/rules";
+
+import "./Login.css";
 
 export default function Login() {
+  const [formState, onInputHandler] = useForm(
+    {
+      username: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  const userLogin = (event) => {
+    event.preventDefault();
+    console.log("User Login");
+  };
+
   return (
-    <div>
-      this is a login page
-    </div>
-  )
+    <>
+      <section className="login-register">
+        <div className="login">
+          <span className="login__title">ورود به حساب کاربری</span>
+
+          <form action="#" className="login-form">
+            <div className="login-form__username">
+              <Input
+                className="login-form__username-input"
+                id="username"
+                type="text"
+                placeholder="email"
+                element="input"
+                validations={[
+                  requiredValidator(),
+                  minValidator(8),
+                  maxValidator(20),
+                  emailValidator(),
+                ]}
+                onInputHandler={onInputHandler}
+              />
+            </div>
+            <div className="login-form__password">
+              <Input
+                element="input"
+                id="password"
+                type="password"
+                className="login-form__password-input"
+                placeholder="password"
+                validations={[
+                  requiredValidator(),
+                  minValidator(8),
+                  maxValidator(18),
+                ]}
+                onInputHandler={onInputHandler}
+              />
+            </div>
+            <Button
+              className={`login-form__btn ${
+                formState.isFormValid
+                  ? "login-form__btn-success"
+                  : "login-form__btn-error"
+              }`}
+              type="submit"
+              onClick={userLogin}
+              disabled={!formState.isFormValid}>
+              <i className="login-form__btn-icon fas fa-sign-out-alt"></i>
+              <span className="login-form__btn-text">submit</span>
+            </Button>
+ 
+          </form>
+
+
+
+          <div className="login__new-member">
+          <span className="login__new-member-text">do you have account ?</span>
+          <Link className="login__new-member-link" to="/register">
+            sing in
+          </Link>
+        </div>
+        </div>
+
+        
+      </section>
+    </>
+  );
 }
